122f6990be55d3ce15509914cd166f6a674ec51b
Ashin <86758582@qq.com>
2021-05-10 18:41:50 +0800

    Gateway-server commit

--- search_front/js/base.js
+++ search_front/js/base.js
@@ -1,18 +1,18 @@
-// var baseUrl = "http://localhost:10010/"; // 公共路径
+var baseUrl = "http://localhost:10010/"; // 公共路径
 var urls = {
-    // search_goods : baseUrl + "search-service/search",
-    // find_good_byid : baseUrl + "goods-service/goods/",
-    // find_order_info_byid: baseUrl + "order-service/order/submitInfo/",
-    // submit_order : baseUrl + "order-service/order/submitOrder",
-    // find_order_byid: baseUrl + "order-service/order/findById/",
-    // pay_order_success: baseUrl + "order-service/order/paySuccess/",
+    search_goods : baseUrl + "search-service/search",
+    find_good_byid : baseUrl + "goods-service/goods/",
+    find_order_info_byid: baseUrl + "order-service/order/submitInfo/",
+    submit_order : baseUrl + "order-service/order/submitOrder",
+    find_order_byid: baseUrl + "order-service/order/findById/",
+    pay_order_success: baseUrl + "order-service/order/paySuccess/",
 
-    search_goods : "http://localhost:9201/search",
-    find_good_byid : "http://localhost:9101/goods/",
-    find_order_info_byid: "http://localhost:9401/order/submitInfo/",
-    submit_order : "http://localhost:9401/order/submitOrder",
-    find_order_byid: "http://localhost:9401/order/findById/",
-    pay_order_success: "http://localhost:9401/order/paySuccess/",
+    // search_goods : "http://localhost:9201/search",
+    // find_good_byid : "http://localhost:9101/goods/",
+    // find_order_info_byid: "http://localhost:9401/order/submitInfo/",
+    // submit_order : "http://localhost:9401/order/submitOrder",
+    // find_order_byid: "http://localhost:9401/order/findById/",
+    // pay_order_success: "http://localhost:9401/order/paySuccess/",
 }
 
 
